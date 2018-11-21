b051338a83636a7b8c1f88e4ae496d7feb03a57f
blockchaindd <penghong93@gmail.com>
2018-11-21 13:43:06 -0800

    UI update

--- imports/ui/Menu.js
+++ imports/ui/Menu.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
-import { Menu, Segment,Popup,Button } from 'semantic-ui-react'
+import { Menu, Segment,Popup,Button, List,Image,Icon,Transition,Form,Header,Grid, SegmentGroup } from 'semantic-ui-react'
 
 export default class MenuExampleSecondaryPointing extends Component {
-  state = { activeItem: 'home' }
+  state = { activeItem: 'home',
+            visible: false
+          }
+ 
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   render() {
     const { activeItem } = this.state
-
+    const { visible } = this.state
     return (
       <div>
         <Menu pointing secondary>
@@ -25,21 +31,53 @@ export default class MenuExampleSecondaryPointing extends Component {
             onClick={this.handleItemClick}
           />
           <Menu.Menu position='right'>
-            <Menu.Item
-              name='logout'
-              active={activeItem === 'logout'}
-              onClick={this.handleItemClick}
-            />
+          <Popup
+                trigger={
+                  <Button animated>
+                  <Button.Content visible>Profile</Button.Content>
+                  <Button.Content hidden>
+                    <Icon name='arrow right' />
+                  </Button.Content>
+                </Button>
+              
+                }
+                content={  <List animated verticalAlign='middle'>
+                <List.Item>
+                  <List.Content>
+                    <List.Header onClick={this.toggleVisibility}>Profile</List.Header>
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    <List.Header onClick={this.toggleVisibility}>wallet</List.Header>
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    <List.Header onClick={this.toggleVisibility}>history</List.Header>
+                  </List.Content>
+                </List.Item>
+              </List>}
+                on='click'
+                position='bottom left'
+             />
           </Menu.Menu>
         </Menu>
-
-        <Segment>
-        <Popup
-                trigger={<Button color='red' icon='flask' content='Activate doomsday device' />}
-                content={<Button color='green' content='Confirm the launch' />}
-                on='click'
-                position='top right'
-             />
+        <Transition visible={visible} animation='scale' duration={500}>
+        <Segment.Group size='small' style={{ left: '30%', position: 'fixed', top: '40%', zIndex: 10 , width : '18', height :'18'}}>
+            <Button position = 'right' color='red' onClick = {this.toggleVisibility} >Close</Button>
+            <Header>Player profile</Header>
+           
+            <Segment.Group horizontal> 
+            <Segment>Amount of EOS :                  </Segment> 
+            <Segment>test</Segment>
+            </Segment.Group>
+            <Segment><p>Amount of Token:                      </p></Segment>
+          </Segment.Group>
+          
+        </Transition>
+        <Segment >
+        
         </Segment>
 
       </div>
