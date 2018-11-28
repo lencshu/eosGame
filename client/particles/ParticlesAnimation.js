ec51a70af47da772757a682d55ed307b8954f4c5
lencshu <lencshu@gmail.com>
2018-11-28 22:41:28 +0100

    Animation modif

--- client/particles/ParticlesAnimation.js
+++ client/particles/ParticlesAnimation.js
@@ -7,7 +7,7 @@ export default class ParticlesAnimation extends Component {
             <Particles
                 params={{
                     particles: {
-                        number: { value: 200, density: { enable: true, value_area: 940 } },
+                        number: { value: 88, density: { enable: true, value_area: 900 } },
                         color: { value: "#ffffff" },
                         shape: {
                             type: "circle",
@@ -21,16 +21,16 @@ export default class ParticlesAnimation extends Component {
                             anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
                         },
                         size: {
-                            value: 2.7000000000000006,
+                            value: 2,
                             random: true,
-                            anim: { enable: true, speed: 36, size_min: 0.1, sync: false }
+                            anim: { enable: false, speed: 36, size_min: 0.1, sync: false }
                         },
                         line_linked: {
                             enable: true,
-                            distance: 270,
+                            distance: 222,
                             color: "#ffffff",
                             opacity: 0.4,
-                            width: 0.6999999999999998
+                            width: 1
                         },
                         move: {
                             enable: true,
@@ -47,20 +47,20 @@ export default class ParticlesAnimation extends Component {
                         detect_on: "canvas",
                         events: {
                             onhover: { enable: true, mode: "grab" },
-                            onclick: { enable: true, mode: "repulse" },
+                            onclick: { enable: true, mode: "bubble" },
                             resize: true
                         },
                         modes: {
                             grab: { distance: 400, line_linked: { opacity: 1 } },
                             bubble: {
                                 distance: 400,
-                                size: 40,
-                                duration: 2,
+                                size: 7,
+                                duration: 0.5,
                                 opacity: 8,
-                                speed: 3
+                                speed: 5
                             },
-                            repulse: { distance: 200, duration: 0.4 },
-                            push: { particles_nb: 4 },
+                            repulse: { distance: 160, duration: 0.2 },
+                            push: { particles_nb: 2 },
                             remove: { particles_nb: 2 }
                         }
                     },
