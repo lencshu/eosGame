b3e7ee4a8afd7fd65fb66cfbaab51c64a90dc2e3
Jinke <ryukg03@gmail.com>
2018-12-01 10:15:20 +0800

    feat: add get coin component

--- client/main.js
+++ client/main.js
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom";
 
 import NaviBar from "../imports/ui/panel/NaviBar";
 import ParticlesAnimation from "../imports/ui/particles/ParticlesAnimation";
+import GetCoin from "../imports/ui/GetCoin/GetCoin";
 
 const rootElement = document.getElementById("navbar");
 ReactDOM.render(<NaviBar />, rootElement);
 
 const partiElement = document.getElementById("particles-js");
 ReactDOM.render(<ParticlesAnimation />, partiElement);
+
+const getCoinElement = document.getElementById("get-coin");
+ReactDOM.render(<GetCoin />, getCoinElement);
